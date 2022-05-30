0693edcbeb0ec272d098318902b8e252b384e719
Piu-Lin <55540481+Piu-Lin@users.noreply.github.com>
2022-05-30 15:57:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
-<script setup>
+<template>
+<!-- <MessageDemo/> -->
+<!-- <Connect/> -->
+<Hero v-show="!LoadComplete"/>
+<FakeConnet  v-show="LoadComplete"  @trigger="trigger"/>
 
+</template>
+<script setup>
+import {ref} from "vue"
 import Connect from './connect.vue'
 import MessageDemo from './components/messageDemo.vue'
 import Home from './pages/Home.vue'
 import Hero from './pages/Hero/Hero.vue'
-
-
+import FakeConnet from './components/FakeConnet.vue';
+import VueRouter from 'vue-router'
+/** 资源加载是否完成 */
+let LoadComplete = ref(false)
+const trigger=()=>{
+    console.log("收到扳机")
+    LoadComplete.value=true
+}
 </script>
-
-<template>
-<!-- <MessageDemo/> -->
-<!-- <Connect/> -->
-<Hero/>
-<div>
-    
-</div>
-</template>
-
 <style>
 body {
     width : 100vw;
---
+++ src/components/FakeConnet.vue
@@ -0,0 +1,30 @@
+<template>
+    虚假链接:
+    
+</template>
+
+<script setup>
+import {onMounted,ref} from "vue"
+onMounted(()=>{
+    text()
+})
+let timer
+
+const emit = defineEmits(["trigger"])
+
+const text=()=>{
+    timer=setTimeout(() => {
+        text2()
+      }, 8000);
+}
+const text2=()=>{
+    clearTimeout(timer);
+    console.log("定时结束")
+    emit("trigger",true)
+}
+
+</script>
+
+<style>
+
+</style>
